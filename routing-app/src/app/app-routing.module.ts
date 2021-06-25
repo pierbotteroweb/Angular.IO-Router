68c92a78e661f2bc1fd90b886fc1f2d96026a9c2
Pier <pierbotteroweb@gmail.com>
2021-06-25 18:29:07 -0300

    Components criados, configuraçã basica de rotas para components criados e observações

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Query } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';//CLI IMPORTA AUTOMATICAMENTE 
 
-const routes: Routes = [];
+import { FirstComponent } from "./first/first.component";
+import { SecondComponent } from "./second/second.component";
+
+// SINTAXE BASICA DE OBJETO EM ITEM DA LISTA DE ROTAS
+const routes: Routes = [
+  {path:'first-component', component: FirstComponent},
+  {path:'second-component', component: SecondComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], //<= APLICA ARRAY DE ROTAS NO ROUTERMODULE
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// ORDEM DAS ROTAS
+// A ORDEM DAS ROTAS É IMPORTANTE, POIS O MODULO ROUTER DO ANGULAR SEMPRE VAI ESCOLHER A PRIMEIRA
+// ROTA CORRESPONDENTE CHAMADA. POR ISSO, ROTAS MAIS ESPECIFICAS DEVEM SER LISTADAS PRIMEIRO,
+// SEGUIDO DE ROTAS MENOS ESPECIFICAS. 
+// LISTAR PRIMEIRO ROTAS COM PATH ESTATICO PRIMEIRO, SEGUIDO DE ROTAS COM PATH VAZIOS
+// CORRESPONDEM A ROTA PADRÃO. E POR ÚLTIMO AS ROTAS WILDCARD POIS ELAS PRECISAM SER ELIMIDADAS NAS TENTATIVAS DE MATCH
+// DAS ROTAS ANTERIORES.
--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
+// ESTE É O APP.MODULE CRIAOD AUTIMATICAMENTE QUANDO
+// CRIAMOS UM PROJETO ANGULAR POR CLI USANDO --ROUTING
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//IMPORTANDO ROUTING MODULE PARA NG-MODULE - PARTE DA CONFIG BASICA DE UM APP COM USO DE ROTAS
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,    
+    AppRoutingModule //IMPORTANDO ROUTING MODULE PARA NG-MODULE - PARTE DA CONFIG BASICA DE UM APP COM USO DE ROTAS
   ],
   providers: [],
   bootstrap: [AppComponent]
