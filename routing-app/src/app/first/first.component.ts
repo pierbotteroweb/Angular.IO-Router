2bc2039fdfd1a9cf97635573ce40780d62fb17c2
Pier <pierbotteroweb@gmail.com>
2021-06-27 19:23:08 -0300

    Obtendo dados da rota usando ActivatedRoute e queryparams

--- routing-app/src/app/first/first.component.ts
+++ routing-app/src/app/first/first.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// A API ACTIVAETOUTE DISPONIBILIZA RECUROS PARA QUE
+// SEJA POSSÍVEL OBTER DADOS DECLARADOS EM UMA URL
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-first',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute) { 
+   }
 
   ngOnInit(): void {
+    // O OBSERVABLE QUERYPARAMS RETORNA O KEY VALUE
+    // DAS INFORMAÇ~EOS DECLARADAS. EXEMPLO:
+    // http://localhost:4200/first-component?name=Pier
+    // TEMOS O OBJETO {name:'Pier'}
+    this.route.queryParams.subscribe(params=>{
+      console.log(params)
+    })
   }
 
 }
